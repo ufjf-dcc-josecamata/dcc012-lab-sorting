1070b532d9e3af6efff66e291968144667137a42
Jose Camata <72111360+josecamata@users.noreply.github.com>
2023-04-03 13:37:31 -0300

    Update performance.cc

--- app/performance.cc
+++ app/performance.cc
@@ -6,16 +6,19 @@ int main(int argc, const char* argv[])
 {
     // TODO: Implementar teste de performance considerando aleatórios
     // Chame as rotinas de ordenação: 
-    // siga::sort1, siga::sort2, siga::sort3, siga::sort4, siga::sort5
+    //  BasicSort, MergeSort, QuickSort, HeapSort, MySort
     // e meça tempo de execução, número de comparações e trocas.
     // Use diferentes tamanhos de vetores para testar.
+    // Verifique o depemepenho usando 
+    //    (1) vetores de inteiros de diferentes tamanhos
+    //    (2) vetores de estudantes de diferentes tamanhos
+    // Observe que a base de dados tem tamanho máximo de 5000 estudantes.
+    // Analise o desempenho considerando as seguintes configurações de entrada:
+    //    (1) dados com distribuição aleatória. (caso médio)
+    //    (2) dados com ordenação descendente (pior caso)
 
-
-
-    // TODO: Implementar test de performance considerando ordenados
-    // Refaça as execuções acima considerando os vetores de entrada ordenados em ordem 
-    // descendente
-
+    //OBS.: Na implementação dos métodos de ordenação, busque por implementações eficientes.
+    //      Indique os resultados obtidos no arquivo REPORT.m
 
     return 0;
 
